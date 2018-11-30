6b1aa54a1ef170041dc236ce11ba52b4b88fad5a
bvalentic <bobvalentic@quickenloans.com>
2018-11-30 14:29:26 -0500

    First commit!

---
+++ RSVP/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using RSVP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSVP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RSVP()
+        {
+            ViewBag.Message = "Répondez s'il vous plaît:";
+
+            return View();
+        }
+
+        public ActionResult RSVPConfirm(Guest guest)
+        {
+            ViewBag.Message = "Thank you for RSVP-ing, " + guest.FirstName + "!";
+
+            if (guest.Attending)
+            {
+                ViewBag.ConfirmMessage = "We'll see you at the big party!";
+            }
+            else
+            {
+                ViewBag.ConfirmMessage = "Sorry you can't make it! We hope to see you at the next one!";
+            }
+
+            return View(guest);
+        }
+
+        public ActionResult BringADish()
+        {
+            ViewBag.Message = "What dish are you bringing?";
+
+            return View();
+        }
+
+        public ActionResult DishConfirm(Dish dish)
+        {
+            return View(dish);
+        }
+    }
+}
---
+++ RSVP/Models/Guest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RSVP.Models
+{
+    public class Guest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool Attending { get; set; }
+        public bool PlusOne { get; set; }
+    }
+
+    public class Dish
+    {
+
+    }
+}
